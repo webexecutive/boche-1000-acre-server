67f1a272679181aaa83c9edc4f9f3b8966b52d83
webexecutive <webexecutive@firstkiss.in>
2026-08-12 12:06:44 +0530

    Update CORS policy to allow dev.boche1000acre.com and wildcard subdomains with OPTIONS preflight support

--- app.js
+++ app.js
@@ -4,16 +4,27 @@ require("dotenv").config()
 const userRoutes = require("./routes/clientRoutes")
 
 const app = express();
+const allowedOrigins = [
+   "http://localhost:5173",
+   "http://localhost:3000",
+   "https://boche1000acre.com",
+   "https://www.boche1000acre.com",
+   "https://dev.boche1000acre.com",
+];
+
 app.use(cors({
-   origin: [
-      "http://localhost:5173",
-      "http://localhost:3000",
-      "https://boche1000acre.com",
-      "https://www.boche1000acre.com"
-   ],
-   methods: ["GET", "POST"],
+   origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) !== -1 || origin.endsWith(".boche1000acre.com")) {
+         return callback(null, true);
+      }
+      return callback(null, false);
+   },
+   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+   allowedHeaders: ["Content-Type", "Authorization"],
    credentials: true
-}))
+}));
+app.options("*", cors());
 app.use(express.json())
 
 app.get("/", (req, res) => {
